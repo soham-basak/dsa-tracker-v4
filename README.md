64c840afbe28c19d6ae87c7594a222dda8f30fb7
Dexter <sohambasak42@gmail.com>
2023-05-07 23:50:14 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#### Demo: https://dsatrackernew.netlify.app/
---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import "../components/Header.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Header() {
+  return (
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky='top' id='navbar-toggler-icon'>
+      <Container>
+        <Navbar.Brand id='brand' href="https://dsa-tracker-home-v2.netlify.app/">DSA Tracker</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+          </Nav>
+          <Nav >
+          <Nav.Link href="https://dsa-tracker-home-v2.netlify.app/" id='items'>Home</Nav.Link>
+            <Nav.Link href="/" id='items'>Dashboard</Nav.Link>
+            <Nav.Link href="/todolist" id='items'>Goals</Nav.Link>
+            <Nav.Link href="/todolist" id='items'>Commiunity</Nav.Link>
+            <Nav.Link href="https://dsaquestion.netlify.app/" id='items'>Daily Questions</Nav.Link>
+            <Nav.Link href="https://dsacompiler.netlify.app/" id='item'>Compiler</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+   
+  );
+}
+
+export default Header;
